3b2c5562ab3545ee2ac330567d17d069a3394f19
Tim Hopper <tdhopper@gmail.com>
2015-01-09 12:30:04 -0500

    access local files too

--- load_machines.py
+++ load_machines.py
@@ -6,7 +6,7 @@ import os.path as path
 def get_machines():
     with open(path.join(path.expanduser("~"), ".hop.json.db"), "r") as f:
         d = json.load(f)
-    return [v.split("$")[2] for v in d.values() if v.startswith("$server$")]
+    return d.iteritems()
 
 
 if __name__ == u"__main__":
@@ -17,7 +17,11 @@ if __name__ == u"__main__":
         query = None
     machines = get_machines()
     if query:
-        machines = wf.filter(query, machines)
-    for machine in machines:
-        wf.add_item(machine, arg=machine, valid=True)
+        machines = wf.filter(query, machines, key=lambda x: " ".join(x))
+    for shortcut, machine in machines:
+        if machine.startswith('$server$'):
+            machine = machine.split("$")[2]
+            wf.add_item("%s - %s" % (shortcut, machine), arg="ssh %s" % machine, valid=True)
+        else:
+            wf.add_item("%s - %s" % (shortcut, machine), arg="cd %s" % machine, valid=True)
     wf.send_feedback()
